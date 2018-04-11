a5b4fb21f8d4a3193110892111f99b1282a0252b
Daniel Martinez <33047490+daniel-mar@users.noreply.github.com>
2018-04-10 23:02:31 -0400

    Add files via upload

---
+++ totalCostScript.js
@@ -0,0 +1,24 @@
+﻿function btnClick() {
+  var itemName = document.getElementById('itemName').value;
+  var itemCost = parseFloat( document.getElementById('itemCost').value );
+  var itemQuantity = parseInt( document.getElementById('itemQuantity').value );
+  var taxRate = parseFloat( document.getElementById('taxRate').value );
+  var total = 0;
+  var msg = "";
+
+  // TAX RATE CONVERSION
+  var adjTax = taxRate / 100;
+
+  // CALC TOTAL
+  total = itemCost * itemQuantity;
+  taxed = total * adjTax;
+
+  total = total + taxed;
+
+  msg += "<div>At $" + itemCost + " each, for " + itemQuantity + " " + itemName + "(s).</div>";
+  msg += "<div>With a " + taxRate + "% Tax Rate.</div>";
+  msg += "<div>Your total cost would be: $" + total.toFixed(2) + ".</div>";
+
+    document.getElementById("output").innerHTML = msg
+
+}
